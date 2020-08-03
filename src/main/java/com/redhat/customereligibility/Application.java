fce69c232f943f8c79ec399c181321fd5a09c475
Sadhana Nandakumar <snandaku@redhat.com>
2020-08-03 12:53:31 -0400

    fixed mysql driver issue

--- src/main/java/com/redhat/customereligibility/Application.java
+++ src/main/java/com/redhat/customereligibility/Application.java
@@ -34,6 +34,8 @@ public class Application {
 
 	// must have a main method spring-boot can run
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(Application.class, args);
 	}
 
